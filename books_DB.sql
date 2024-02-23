84d6ac3f0779426b6b9e8cef30741ae73a81f96c
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-02-23 19:36:09 +0100

    Add files via upload

---
+++ books_DB.sql
@@ -0,0 +1,17 @@
+create table books (
+	id INT,
+	author VARCHAR(120),
+	title VARCHAR(120),
+	subtitle VARCHAR(120),
+	year INT
+);
+insert into books (id, author, title, subtitle, year) values (1, 'Miljenko Jergović', 'Sarajevo Marlboro', NULL, 2004);
+insert into books (id, author, title, subtitle, year) values (2, 'Charles Dickens', 'Oliver Twist', 'The Parish Boy''s Progress', 1838);
+insert into books (id, author, title, subtitle, year) values (3, 'Tilar J. Mazzeo', 'Irena''s Children', 'The Extraordinary Story of the Woman Who Saved 2,500 Children from the Warsaw Ghetto', 2016);
+insert into books (id, author, title, subtitle, year) values (4, 'David Graeber', 'Bullshit Jobs', 'A Theory', 2018);
+insert into books (id, author, title, subtitle, year) values (5, 'Olga Tokarzcuk', 'Primeval and Other Times', NULL, 2010);
+insert into books (id, author, title, subtitle, year) values (6, 'Thomas Hardy', 'Tess of the d'' Urbevilles', 'A Pure Woman Faithfully Presented', 1891);
+insert into books (id, author, title, subtitle, year) values (7, 'Sylvia Plath', 'Ariel', NULL, 1965);
+insert into books (id, author, title, subtitle, year) values (8, 'Toni Morrison', 'Jazz', NULL, 1992);
+insert into books (id, author, title, subtitle, year) values (9, 'Haben Girma', 'Haben', 'The Deafblind Woman Who Conquered Harvard Law', 2019);
+insert into books (id, author, title, subtitle, year) values (10, 'Thomas Mann', 'Buddenbrooks', 'The Decline of a Family', 1901);
