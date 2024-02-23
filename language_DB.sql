669c8795264564738a6179aca9e46e290f483b2e
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-02-23 19:09:06 +0100

    Add files via upload

---
+++ language_DB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE language (
+	id INT PRIMARY KEY,
+	language_name VARCHAR (120)
+
+);
+INSERT INTO language (id, language_name) VALUES (1, 'English');
+INSERT INTO language (id, language_name) VALUES (2, 'Dutch');
+INSERT INTO language (id, language_name) VALUES (3, 'Russian');
+INSERT INTO language (id, language_name) VALUES (4, 'Polish');
+INSERT INTO language (id, language_name) VALUES (5, 'Croatian');
