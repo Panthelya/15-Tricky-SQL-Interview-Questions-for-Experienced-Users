bbd7f001eea9684368d53dddb2c6d227eba8eabe
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-02-23 19:30:11 +0100

    Add files via upload

---
+++ salaries_DB.sql
@@ -0,0 +1,22 @@
+create table salaries (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	department VARCHAR(50),
+	salary DECIMAL(7,2)
+);
+insert into salaries (id, first_name, last_name, department, salary) values (1, 'Benny', 'Gilhespy', 'Sales', 5293.47);
+insert into salaries (id, first_name, last_name, department, salary) values (2, 'Benetta', 'Featherstonhaugh', 'Engineering', 2214.55);
+insert into salaries (id, first_name, last_name, department, salary) values (3, 'Karla', 'Stiell', 'Sales', 2070.45);
+insert into salaries (id, first_name, last_name, department, salary) values (4, 'Sissie', 'Seabon', 'Accounting', 5077.42);
+insert into salaries (id, first_name, last_name, department, salary) values (5, 'Genna', 'Beche', 'Accounting', 7451.65);
+insert into salaries (id, first_name, last_name, department, salary) values (6, 'Kirsten', 'Fernandez', 'Engineering', 7533.13);
+insert into salaries (id, first_name, last_name, department, salary) values (7, 'Pen', 'Fredy', 'Sales', 7867.54);
+insert into salaries (id, first_name, last_name, department, salary) values (8, 'Tish', 'Calderbank', 'Sales', 4103.19);
+insert into salaries (id, first_name, last_name, department, salary) values (9, 'Gallard', 'Philipet', 'Accounting', 7220.06);
+insert into salaries (id, first_name, last_name, department, salary) values (10, 'Walsh', 'Kleinholz', 'Accounting', 4000.18);
+insert into salaries (id, first_name, last_name, department, salary) values (11, 'Carce', 'Wilkenson', 'Accounting', 3991.0);
+insert into salaries (id, first_name, last_name, department, salary) values (12, 'Tami', 'Langrish', 'Sales', 5588.34);
+insert into salaries (id, first_name, last_name, department, salary) values (13, 'Shayne', 'Dearn', 'Engineering', 2785.92);
+insert into salaries (id, first_name, last_name, department, salary) values (14, 'Merla', 'Ingilson', 'Engineering', 2980.36);
+insert into salaries (id, first_name, last_name, department, salary) values (15, 'Keely', 'Patifield', 'Sales', 2877.92);
